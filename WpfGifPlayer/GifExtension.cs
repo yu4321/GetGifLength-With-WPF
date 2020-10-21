4cf54e1e27c4ec076112362a94e4d542c3ae5e8d
Ryu Youngseok <ryu.youngseok0512@mediasol.kr>
2020-10-21 16:55:35 +0900

    초기 커밋.

---
+++ WpfGifPlayer/GifExtension.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace WpfGifPlayer
+{
+    public static class GifExtension
+    {
+        /// <summary>
+        /// https://stackoverflow.com/questions/47343230/how-do-you-get-the-duration-of-a-gif-file-in-c
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="fps"></param>
+        /// <returns></returns>
+        public static TimeSpan? GetGifDuration(this Image image, int fps = 60)
+        {
+            var minimumFrameDelay = (1000.0 / fps);
+            if (!image.RawFormat.Equals(ImageFormat.Gif)) return null;
+            if (!ImageAnimator.CanAnimate(image)) return null;
+
+            var frameDimension = new FrameDimension(image.FrameDimensionsList[0]);
+
+            var frameCount = image.GetFrameCount(frameDimension);
+            var totalDuration = 0;
+
+            for (var f = 0; f < frameCount; f++)
+            {
+                var delayPropertyBytes = image.GetPropertyItem(20736).Value;
+                var frameDelay = BitConverter.ToInt32(delayPropertyBytes, f * 4) * 10;
+                totalDuration += (frameDelay < minimumFrameDelay ? (int)minimumFrameDelay : frameDelay);
+            }
+
+            return TimeSpan.FromMilliseconds(totalDuration);
+        }
+
+        public static TimeSpan GetGifDuration(this System.Windows.Media.Imaging.BitmapImage image, int fps = 60)
+        {
+            var bm = new Bitmap(image.StreamSource);
+            var val = bm.GetGifDuration();
+            return val.HasValue ? val.Value : default(TimeSpan);
+        }
+
+    }
+}
---
+++ WpfGifPlayer/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using XamlAnimatedGif;
+
+namespace WpfGifPlayer
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private int count = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile_Click(object sender, RoutedEventArgs e)
+        {
+            var nextUri = OpenFile("Gif이미지를 선택해주세요", "Image (*.gif)|*.gif");
+            if (nextUri != string.Empty)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    UnloadFile();
+                    LoadFile(nextUri);
+                });
+            }
+        }
+
+        private void UnloadFile()
+        {
+            try
+            {
+                holder.Source = null;
+                var ss = AnimationBehavior.GetSourceStream(holder);
+                ss.Dispose();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unload File Failed - " + e);
+            }
+
+        }
+
+        private void LoadFile(string nextUri)
+        {
+            var ms = new MemoryStream(File.ReadAllBytes(nextUri));
+            var img1 = new BitmapImage();
+            img1.BeginInit();
+            img1.CacheOption = BitmapCacheOption.OnLoad;
+            img1.StreamSource = ms;
+            img1.EndInit();
+
+            var duration = GifExtension.GetGifDuration(img1);
+            AnimationBehavior.SetSourceStream(holder, img1.StreamSource);
+            StartRepeatChecker(duration);
+        }
+
+        public string OpenFile(string caption, string filter = "All files (*.*)|*.*")
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.CurrentDirectory;// Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            dialog.Title = caption;
+            dialog.Filter = filter;
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            //dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() == true) return dialog.FileName;
+
+            return string.Empty;
+        }
+
+        private void StartRepeatChecker(TimeSpan sp)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                timer = null;
+            }
+
+            count = 0;
+            duration.Text = sp.ToString(@"mm\:ss\.ff");
+            repeated.Text = "0";
+
+            timer = new DispatcherTimer();
+            timer.Interval = sp;
+
+            timer.Tick += (s, e) =>
+            {
+                count++;
+                Console.WriteLine($"completed {count}");
+                repeated.Text = count.ToString();
+            };
+
+            timer.Start();
+        }
+    }
+}
